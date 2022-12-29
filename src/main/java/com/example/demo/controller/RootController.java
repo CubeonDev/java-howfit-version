80de79c67dd0351fd25adcaed5fb723ed292124e
linus <linus.choi@shinhan-cubeon.com>
2022-12-29 17:52:03 +0900

    첫 커밋

--- src/main/java/com/example/demo/controller/RootController.java
+++ src/main/java/com/example/demo/controller/RootController.java
@@ -13,7 +13,7 @@ public class RootController {
     public ResponseEntity<?> rootControl(
     ) {
         return ResponseEntity.ok(
-                "{\"msgType\":11006,\"errCode\":0,\"message\":\"\",\"sessToken\":\"\",\"data\":{\"environment\":40,\"key\":\"68c4e9c64453f81c5578badc3e47bcc8\",\"apiUrl\":\"https://chathowfitkr.azurewebsites.net\",\"webSocketUrl\":\"wss://chathowfitkr.azurewebsites.net\",\"versions\":[\"kt01.00.03\",\"kt01.00.02\",\"kt01.00.01\",\"kt01.00.00\",\"kt01.00.04\",\"a1.0.5\",\"i1.1.16\",\"a1.0.6\",\"i1.1.17\"],\"review\":[]}}"
+                "{\"msgType\":11006,\"errCode\":0,\"message\":\"\",\"sessToken\":\"\",\"data\":{\"environment\":40,\"key\":\"68c4e9c64453f81c5578badc3e47bcc8\",\"apiUrl\":\"https://chathowfitkr.azurewebsites.net\",\"webSocketUrl\":\"wss://chathowfitkr.azurewebsites.net\",\"versions\":[\"kt01.00.03\",\"kt01.00.02\",\"kt01.00.01\",\"kt01.00.00\",\"kt01.00.04\",\"a1.0.0\",\"i1.1.16\",\"a1.0.0\",\"i1.1.17\"],\"review\":[]}}"
         );
     }
 }
